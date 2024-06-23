519857174bdbae76e92e8ba24df4e4b804800379
Md Asif Nawaz <mdasifnawaz545@gmail.com>
2024-06-23 08:16:49 +0530

    Initial files are added

---
+++ one.js
@@ -0,0 +1,5 @@
+const mathFunc=require("./two")
+console.log(mathFunc.sum(3,2))
+console.log(mathFunc.sub(3,2))
+
+for(let x of process.argv) console.log(x+"aa");
---
+++ two.js
@@ -0,0 +1,8 @@
+const obj={
+    sum:(a,b)=>(a+b),
+    sub(a,b){return a-b},
+    mul(a,b){return a*b},
+    div(a,b){return a/b}
+}
+
+module.exports = obj;
